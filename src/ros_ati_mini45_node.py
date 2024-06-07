667ea02df95220f0ac2dab248560ae4426bbda22
aelang <a.e.lang@student.tudelft.nl>
2024-06-07 12:00:34 +0200

    ?

--- src/ros_ati_mini45_node.py
+++ src/ros_ati_mini45_node.py
@@ -1,10 +1,10 @@
 import rospy
 from ros_ati_mini45.msg import ForceMeasurement
 from ros_ati_mini45.msg import ZeroingDuration
-import scripts.ft_interface as fif
+import ros_ati_mini45.scripts.ft_interface as fif
 import time
 import std_msgs.msg
-import scripts.preprocessing as pre
+import ros_ati_mini45.scripts.preprocessing as pre
 import numpy as np
 import struct
 import math
